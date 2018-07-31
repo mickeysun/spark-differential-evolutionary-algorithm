2f0c0fadfaa3be18b660e2403daf70800e04f5e6
mickeysun <1119691931@qq.com>
2018-07-31 10:22:17 +0800

    Update Individual.scala

--- src/main/scala/com/mickeysun/bigpaper/population/Individual.scala
+++ src/main/scala/com/mickeysun/bigpaper/population/Individual.scala
@@ -1,14 +1,11 @@
 package com.mickeysun.bigpaper.population
 
-/**
-  * Created by mickeysun on 17-7-21.
-  */
+/*
+ * Created by mickeysun on 17-7-21.
+ */
 
-//定义Individual
-//后期根据需要进行字段增加与修改
 case class Individual(index:Int,
                       chromosome:Array[Double],
-
                       fitnessScore: Option[Double],
                       mutation:Int,
                       FandCR:(Double,Double),
@@ -18,33 +15,12 @@ case class Individual(index:Int,
                       hpd:Option[Array[Double]]
                      ) extends Serializable{
 
-
-
-
-  def setIndex(value:Int):Individual = copy(index = value)
-
-  def setChrom(value:Array[Double]) = copy(chromosome = value)
-
-  def setFitness(value:Double) = copy(fitnessScore = Some(value))
-
-  def setMutation(value:Int) = copy(mutation = value)
-
-  def setState(value:Int) = copy( state = value)
-
-  def setFandCR(value:(Double,Double)) = copy(FandCR = value)
-
-
-
-
   override def toString: String =
    // "{\"chromosome\": ["+ chromosome.toArray.mkString(",") + "]"+
       "\n\"index\": " + index +
       ",\n\"fitnessScore\": " + fitnessScore.get
      // ",\n\"spd\": [" + spd.get.mkString(",") + "]" +
       //",\n\"hpd\": [" + hpd.get.mkString(",") + "]"
-
-
-
 }
 
 
